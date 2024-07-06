636f0acf5e906998c0cfad0cb861fad21d5e7ba4
Aung Kyaw Phyo <Dev.aungkyawphyo@gmail.com>
2024-07-07 02:05:47 +0630

    Initial Commit

---
+++ Sample_APIwithMovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sample_APIwithMovieApp
+//
+//  Created by Aung Kyaw Phyo on 07/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

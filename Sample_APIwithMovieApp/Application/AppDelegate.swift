931b98ca788eee05011bd50004a47ac493e34c77
Aung Kyaw Phyo <Dev.aungkyawphyo@gmail.com>
2024-07-08 01:21:47 +0630

    Upload share

--- Sample_APIwithMovieApp/Application/AppDelegate.swift
+++ Sample_APIwithMovieApp/Application/AppDelegate.swift
@@ -14,7 +14,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        GIDSignIn.sharedInstance.restorePreviousSignIn { user, error in
+            if error != nil || user == nil {
+                // Show the app's signed-out state.
+            } else {
+                // Show the app's signed-in state.
+            }
+        }
         return true
     }
 
@@ -32,6 +38,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        var handled: Bool
+        handled = GIDSignIn.sharedInstance.handle(url)
+        if handled {
+            return true
+        }
+        // Handle other custom URL types.
+        
+        // If not handled by this app, return false.
+        return false
+    }
+    
 }
 
--- Sample_APIwithMovieApp/Modules/LoginScreenModule/LoginScreenViewController.swift
+++ Sample_APIwithMovieApp/Modules/LoginScreenModule/LoginScreenViewController.swift
@@ -6,8 +6,18 @@
 //
 
 import UIKit
+import GoogleSignIn
 
 class LoginScreenViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var emailErrorLabel: UILabel!
+    @IBOutlet weak var passwordErrorLabel: UILabel!
+    
+    @IBOutlet weak var loginInWithGoogleBtn: UIButton!
+    @IBOutlet weak var loginInWithFbBtn: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,3 +27,46 @@ class LoginScreenViewController: UIViewController {
     
 
 }
+
+// MARK: - Extensions
+
+// MARK: - UITextFieldDelegate
+extension LoginScreenViewController: UITextFieldDelegate {
+    
+}
+
+// MARK: - @IBAction
+extension LoginScreenViewController {
+    @IBAction func didTappedLoginBtnAction(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func didTappedGoogleLoginBtnAction(_ sender: UIButton) {
+        GIDSignIn.sharedInstance.signIn(withPresenting: self) { GIDSignInResult, error in
+            self.loginInWithGoogleBtn.isHidden = false
+            self.loginInWithFbBtn.isHidden = false
+            guard error == nil else { return }
+            
+            // If sign in succeeded, display the app's main content View.
+            guard let signInResult = GIDSignInResult else { return }
+            let user = signInResult.user
+            let emailAddress = user.profile?.email
+            let fullName = user.profile?.name
+            let familyName = user.profile?.familyName
+            let profilePicUrl = user.profile?.imageURL(withDimension: 320)
+            
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let vc = storyboard.instantiateViewController(withIdentifier: "HomeScreenViewController")
+            vc.modalTransitionStyle = .crossDissolve
+            vc.modalPresentationStyle = .fullScreen
+            self.present(vc, animated: true)
+        }
+    }
+    
+    @IBAction func didTappedFbLoginBtnAction(_ sender: UIButton) {
+        
+    }
+}
+
+//extension LoginScreenViewController:
+
